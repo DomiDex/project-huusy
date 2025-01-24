edbbca9b2919623c041afb3e2274363dc06c9d8f
Dominique Degottex <75885483+DomiDex@users.noreply.github.com>
2025-01-24 16:08:27 +0700

    Remove SpeedInsightsProvider from layout and delete its implementation to streamline the application structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import LenisProvider from '@/components/ui/LenisProvider';
-import { SpeedInsightsProvider } from '@/providers/SpeedInsightsProvider';
 
 import '@/styles/globals.css';
 
@@ -21,7 +20,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang='en' className={inter.className}>
       <body>
         <LenisProvider>{children}</LenisProvider>
-        <SpeedInsightsProvider />
       </body>
     </html>
   );
